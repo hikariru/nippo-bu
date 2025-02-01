f98db26b261726c1597c62c3b6818e0724f3c9a9
Uchiru <7587023+hikariru@users.noreply.github.com>
2025-02-01 10:47:56 +0900

    refactor: Procfileとstart.shを整理し不要なコードを削除

--- start.sh
+++ start.sh
@@ -3,13 +3,14 @@
 USERNAME=${BASIC_AUTH_USERNAME:-default_username}
 PASSWORD=${BASIC_AUTH_PASSWORD:-default_password}
 
-mkdir -p /etc/nginx/
 mkdir -p /tmp/nginx/conf/
 mkdir -p /tmp/nginx/logs/
 
-echo "$USERNAME:$(openssl passwd -apr1 $PASSWORD)" > /etc/nginx/.htpasswd_env
+echo "$USERNAME:$(openssl passwd -apr1 $PASSWORD)" > /tmp/nginx/.htpasswd_env
+
+cp config/nginx.conf.erb /tmp/nginx/conf/nginx.conf
 
 # node server.js &
 
 # NGINXをスタート
-exec nginx -g "daemon off;"
+nginx -p /tmp/nginx/ -c /tmp/nginx/conf/nginx.conf -g "daemon off;"
