4542c57de25a2cfd3dc0ce6390233b70215b766f
Uchiru <7587023+hikariru@users.noreply.github.com>
2025-02-08 10:23:55 +0900

    feat: ポート設定を環境変数で指定可能に

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import {sveltekit} from '@sveltejs/kit/vite';
 import {defineConfig} from 'vite';
 
 export default defineConfig({
-    plugins: [sveltekit()]
+    plugins: [sveltekit()],
+    server: {
+      port: parseInt(process.env.PORT || '3000'),
+    },
 });
