99f95e8bb42e1aa83993fdd70ccc119ab70ea25f
Uchiru <7587023+hikariru@users.noreply.github.com>
2025-02-08 12:46:35 +0900

    refactor: simplify nginx setup and authentication process

--- start.sh
+++ start.sh
@@ -3,16 +3,6 @@
 USERNAME=${BASIC_AUTH_USERNAME:-default_username}
 PASSWORD=${BASIC_AUTH_PASSWORD:-default_password}
 
-mkdir -p /tmp/nginx/conf/
-mkdir -p /tmp/nginx/logs/
+echo "$USERNAME:$(openssl passwd -apr1 $PASSWORD)" > app/.htpasswd
 
-echo "$USERNAME:$(openssl passwd -apr1 $PASSWORD)" > /tmp/nginx/.htpasswd_env
-
-erb config/nginx.conf.erb > /tmp/nginx/conf/nginx.conf
-
-export HOST=127.0.0.1
-export PORT=8000
-
-npm start &
-
-nginx -p /tmp/nginx/ -c /tmp/nginx/conf/nginx.conf -g "daemon off;"
+bin/start-nginx npm start
