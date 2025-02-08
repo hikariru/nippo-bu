1ef5c7a0f615a88d1c244c9dd84622ae512afddb
Uchiru <7587023+hikariru@users.noreply.github.com>
2025-02-08 18:02:02 +0900

    refactor: 認証設定を簡素化しnginxのデバッグ版を使用

--- start.sh
+++ start.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-USERNAME=${BASIC_AUTH_USERNAME:-default_username}
-PASSWORD=${BASIC_AUTH_PASSWORD:-default_password}
-
 chmod 1777 /tmp
 
-echo "$USERNAME:$(openssl passwd -apr1 $PASSWORD)" > /tmp/.htpasswd
+echo "${BASIC_AUTH_USERNAME}:$(openssl passwd -apr1 ${BASIC_AUTH_PASSWORD})" > /tmp/.htpasswd
 
-if [ -f /tmp/.htpasswd ]; then
-    echo "/tmp/.htpasswd successfully created. Content:"
-    cat /tmp/.htpasswd
-else
-    echo "Failed to create /tmp/.htpasswd"
-    exit 1
-fi
-
-bin/start-nginx npm start
+bin/start-nginx-debug npm start
