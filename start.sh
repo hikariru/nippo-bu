e2a1544954e10f06a8113c171467c2511aed6107
Uchiru <7587023+hikariru@users.noreply.github.com>
2025-02-08 17:18:50 +0900

    fix: /tmpディレクトリのパーミッション設定を追加

--- start.sh
+++ start.sh
@@ -3,6 +3,8 @@
 USERNAME=${BASIC_AUTH_USERNAME:-default_username}
 PASSWORD=${BASIC_AUTH_PASSWORD:-default_password}
 
+chmod 1777 /tmp
+
 echo "$USERNAME:$(openssl passwd -apr1 $PASSWORD)" > /tmp/.htpasswd
 
 bin/start-nginx npm start
