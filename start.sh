4303f78272eabfde86c93e584cb7b051e9b19fb9
Uchiru <7587023+hikariru@users.noreply.github.com>
2025-02-08 13:09:07 +0900

    fix: .htpasswdのパス設定を修正

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 USERNAME=${BASIC_AUTH_USERNAME:-default_username}
 PASSWORD=${BASIC_AUTH_PASSWORD:-default_password}
 
-echo "$USERNAME:$(openssl passwd -apr1 $PASSWORD)" > .htpasswd
+echo "$USERNAME:$(openssl passwd -apr1 $PASSWORD)" > /tmp/.htpasswd
 
 bin/start-nginx npm start
